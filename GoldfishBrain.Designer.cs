39c25e36b4d5a4f2db3d9770290cbf8b540af2a2
Ben <54918549+BenJerke@users.noreply.github.com>
2023-02-02 12:37:32 -0500

    initial commit

---
+++ GoldfishBrain.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace GoldfishBrain
+{
+    partial class GoldfishBrain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GoldfishBrain));
+            this.submitButton = new System.Windows.Forms.Button();
+            this.activeTaskLabel = new System.Windows.Forms.Label();
+            this.taskEntryTextBox = new System.Windows.Forms.TextBox();
+            this.projectEntryTextBox = new System.Windows.Forms.TextBox();
+            this.taskLabel = new System.Windows.Forms.Label();
+            this.projectLabel = new System.Windows.Forms.Label();
+            this.activeProjectLabel = new System.Windows.Forms.Label();
+            this.completeTaskButton = new System.Windows.Forms.Button();
+            this.cancelTaskButton = new System.Windows.Forms.Button();
+            this.submitTaskAndSwitchButton = new System.Windows.Forms.Button();
+            this.switchTaskButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(54, 149);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(121, 23);
+            this.submitButton.TabIndex = 7;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // activeTaskLabel
+            // 
+            this.activeTaskLabel.AutoEllipsis = true;
+            this.activeTaskLabel.AutoSize = true;
+            this.activeTaskLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.activeTaskLabel.Enabled = false;
+            this.activeTaskLabel.Font = new System.Drawing.Font("Bahnschrift", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.activeTaskLabel.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.activeTaskLabel.Location = new System.Drawing.Point(8, 9);
+            this.activeTaskLabel.Name = "activeTaskLabel";
+            this.activeTaskLabel.Size = new System.Drawing.Size(104, 20);
+            this.activeTaskLabel.TabIndex = 10;
+            this.activeTaskLabel.Text = "<<active task>>";
+            this.activeTaskLabel.UseCompatibleTextRendering = true;
+            this.activeTaskLabel.Click += new System.EventHandler(this.activeTaskLabel_Click_1);
+            // 
+            // taskEntryTextBox
+            // 
+            this.taskEntryTextBox.Location = new System.Drawing.Point(54, 97);
+            this.taskEntryTextBox.Name = "taskEntryTextBox";
+            this.taskEntryTextBox.Size = new System.Drawing.Size(237, 20);
+            this.taskEntryTextBox.TabIndex = 4;
+            // 
+            // projectEntryTextBox
+            // 
+            this.projectEntryTextBox.Location = new System.Drawing.Point(54, 123);
+            this.projectEntryTextBox.Name = "projectEntryTextBox";
+            this.projectEntryTextBox.Size = new System.Drawing.Size(237, 20);
+            this.projectEntryTextBox.TabIndex = 6;
+            // 
+            // taskLabel
+            // 
+            this.taskLabel.AutoSize = true;
+            this.taskLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taskLabel.Location = new System.Drawing.Point(14, 97);
+            this.taskLabel.Name = "taskLabel";
+            this.taskLabel.Size = new System.Drawing.Size(34, 13);
+            this.taskLabel.TabIndex = 5;
+            this.taskLabel.Text = "Task:";
+            // 
+            // projectLabel
+            // 
+            this.projectLabel.AutoSize = true;
+            this.projectLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.projectLabel.Location = new System.Drawing.Point(5, 123);
+            this.projectLabel.Name = "projectLabel";
+            this.projectLabel.Size = new System.Drawing.Size(43, 13);
+            this.projectLabel.TabIndex = 6;
+            this.projectLabel.Text = "Project:";
+            // 
+            // activeProjectLabel
+            // 
+            this.activeProjectLabel.AutoEllipsis = true;
+            this.activeProjectLabel.AutoSize = true;
+            this.activeProjectLabel.Enabled = false;
+            this.activeProjectLabel.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.activeProjectLabel.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.activeProjectLabel.Location = new System.Drawing.Point(8, 45);
+            this.activeProjectLabel.Name = "activeProjectLabel";
+            this.activeProjectLabel.Size = new System.Drawing.Size(125, 20);
+            this.activeProjectLabel.TabIndex = 9;
+            this.activeProjectLabel.Text = "<<active project>>";
+            this.activeProjectLabel.UseCompatibleTextRendering = true;
+            // 
+            // completeTaskButton
+            // 
+            this.completeTaskButton.Location = new System.Drawing.Point(54, 68);
+            this.completeTaskButton.Name = "completeTaskButton";
+            this.completeTaskButton.Size = new System.Drawing.Size(75, 23);
+            this.completeTaskButton.TabIndex = 1;
+            this.completeTaskButton.Text = "Done";
+            this.completeTaskButton.UseVisualStyleBackColor = true;
+            this.completeTaskButton.Click += new System.EventHandler(this.completeTaskButton_Click);
+            // 
+            // cancelTaskButton
+            // 
+            this.cancelTaskButton.Location = new System.Drawing.Point(216, 68);
+            this.cancelTaskButton.Name = "cancelTaskButton";
+            this.cancelTaskButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelTaskButton.TabIndex = 3;
+            this.cancelTaskButton.Text = "Cancel";
+            this.cancelTaskButton.UseVisualStyleBackColor = true;
+            this.cancelTaskButton.Click += new System.EventHandler(this.cancelTaskButton_Click);
+            // 
+            // submitTaskAndSwitchButton
+            // 
+            this.submitTaskAndSwitchButton.Location = new System.Drawing.Point(181, 149);
+            this.submitTaskAndSwitchButton.Name = "submitTaskAndSwitchButton";
+            this.submitTaskAndSwitchButton.Size = new System.Drawing.Size(110, 23);
+            this.submitTaskAndSwitchButton.TabIndex = 8;
+            this.submitTaskAndSwitchButton.Text = "Submit and Switch";
+            this.submitTaskAndSwitchButton.UseVisualStyleBackColor = true;
+            this.submitTaskAndSwitchButton.Click += new System.EventHandler(this.submitTaskAndSwitchButton_Click);
+            // 
+            // switchTaskButton
+            // 
+            this.switchTaskButton.Location = new System.Drawing.Point(135, 68);
+            this.switchTaskButton.Name = "switchTaskButton";
+            this.switchTaskButton.Size = new System.Drawing.Size(75, 23);
+            this.switchTaskButton.TabIndex = 2;
+            this.switchTaskButton.Text = "Switch";
+            this.switchTaskButton.UseVisualStyleBackColor = true;
+            this.switchTaskButton.Click += new System.EventHandler(this.switchTaskButton_Click);
+            // 
+            // GoldfishBrain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 180);
+            this.Controls.Add(this.submitTaskAndSwitchButton);
+            this.Controls.Add(this.cancelTaskButton);
+            this.Controls.Add(this.switchTaskButton);
+            this.Controls.Add(this.completeTaskButton);
+            this.Controls.Add(this.activeProjectLabel);
+            this.Controls.Add(this.projectLabel);
+            this.Controls.Add(this.taskLabel);
+            this.Controls.Add(this.projectEntryTextBox);
+            this.Controls.Add(this.taskEntryTextBox);
+            this.Controls.Add(this.activeTaskLabel);
+            this.Controls.Add(this.submitButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "GoldfishBrain";
+            this.ShowIcon = false;
+            this.Text = "Goldfish Brain";
+            this.TopMost = true;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.GoldfishBrain_FormClosed);
+            this.Load += new System.EventHandler(this.GoldfishBrain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label activeTaskLabel;
+        private System.Windows.Forms.TextBox taskEntryTextBox;
+        private System.Windows.Forms.TextBox projectEntryTextBox;
+        private System.Windows.Forms.Label taskLabel;
+        private System.Windows.Forms.Label projectLabel;
+        private System.Windows.Forms.Label activeProjectLabel;
+        private System.Windows.Forms.Button completeTaskButton;
+        private System.Windows.Forms.Button cancelTaskButton;
+        private System.Windows.Forms.Button submitTaskAndSwitchButton;
+        private System.Windows.Forms.Button switchTaskButton;
+    }
+}
+
---
+++ GoldfishBrain.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GoldfishBrainLibrary;
+
+namespace GoldfishBrain
+{
+    public partial class GoldfishBrain : Form
+    {
+        //new session
+        private Session session = new Session();
+        //load data from last session into this one
+        public GoldfishBrain()
+        {
+            InitializeComponent();
+            StartSessionTimer(session);
+            WriteLabels(session);
+        }
+        private void GoldfishBrain_Load(object sender, EventArgs e)
+        {
+            //pull task and project lists + associated data from save file
+        }
+        private void GoldfishBrain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            StopActiveTaskTimer(session);
+            StopSessionTimer(session);
+            session.EndDatetime = DateTime.Now;
+            Report(session);
+            return;
+        }
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            SubmitNewTask(session);
+            StartActiveTaskTimer(session);
+            WriteLabels(session);
+            return;
+        }
+        private void completeTaskButton_Click(object sender, EventArgs e)
+        {
+            StopActiveTaskTimer(session);
+            CompleteActiveTask(session);
+            SwitchActiveTask(session);
+            StartActiveTaskTimer(session);
+            WriteLabels(session);
+            return;
+        }
+        private void switchTaskButton_Click(object sender, EventArgs e)
+        {
+            SwitchActiveTask(session);
+            //RunActiveTaskTimer(session);
+            WriteLabels(session);
+            return;
+        }
+        private void cancelTaskButton_Click(object sender, EventArgs e)
+        {
+            StopActiveTaskTimer(session);
+            CancelActiveTask(session);
+            SwitchActiveTask(session);
+            StartActiveTaskTimer(session);
+            WriteLabels(session);
+            return;
+        }
+        private void SubmitNewTask(Session CurrentSession)
+        {
+            if (taskEntryTextBox.Text.Length > 0 && projectEntryTextBox.Text.Length > 0)
+            {
+                //create new task and project, put task on project
+                Project NewPrj = new Project(projectEntryTextBox.Text);
+                WorkTask NewTask = new WorkTask(taskEntryTextBox.Text, NewPrj);
+
+                CurrentSession.IncompleteTasks.Add(NewTask);
+
+                taskEntryTextBox.Clear();
+
+                if (GetActiveTask() == null)
+                {
+                    CurrentSession.ActiveTask = CurrentSession.IncompleteTasks[0];
+                    CurrentSession.IncompleteTasks.RemoveAt(0);
+                }
+                session = CurrentSession;
+                return;
+            }
+            else if (taskEntryTextBox.Text.Length > 0)
+            {
+                WorkTask NewTask = new WorkTask(taskEntryTextBox.Text, null);
+                //Create new task
+                session.IncompleteTasks.Add(NewTask);
+
+                if (GetActiveTask() == null)
+                {
+                    CurrentSession.ActiveTask = CurrentSession.IncompleteTasks[0];
+                    session.IncompleteTasks.RemoveAt(0);
+                }
+                taskEntryTextBox.Clear();
+                session = CurrentSession;
+                return;
+
+
+            }
+            else
+            {
+                //If nothing's entered, get mad
+                MessageBox.Show("Enter a task.");
+                return;
+            }
+        }
+        private void SubmitAndSwitch(Session CurrentSession)
+        {
+            SubmitNewTask(CurrentSession);
+            if (CurrentSession.IncompleteTasks.Count >= 1)
+            {
+                WorkTask Switcher = CurrentSession.IncompleteTasks.Last();
+                CurrentSession.IncompleteTasks.Insert(0, Switcher);
+                CurrentSession.IncompleteTasks.RemoveAt(CurrentSession.IncompleteTasks.Count - 1);
+                CurrentSession.IncompleteTasks.Add(CurrentSession.ActiveTask);
+                CurrentSession.ActiveTask = CurrentSession.IncompleteTasks[0];
+                CurrentSession.IncompleteTasks.RemoveAt(0);
+                session = CurrentSession;
+                return;
+            }
+            else //should just work if we're submitting the first item.
+            return;
+        }
+        private WorkTask GetActiveTask()
+        {
+            if (session.ActiveTask != null)
+            {
+                return session.ActiveTask;
+            }
+            else return null;
+        }
+        private void StartActiveTaskTimer(Session CurrentSession)
+        {
+            if (GetActiveTask() != null)
+            {
+                WorkTask task = GetActiveTask();
+                DateTime StartInstant = DateTime.Now;
+                if (task.ElapsedTime == TimeSpan.Zero)
+                {
+                    task.StartDatetime = StartInstant;
+                    return;
+                }
+            }
+            else return;
+        }
+        private void StopActiveTaskTimer(Session CurrentSession)
+        {
+            if (GetActiveTask() != null)
+            {
+                WorkTask task = GetActiveTask();
+                task.ElapsedTime = task.ElapsedTime + (DateTime.Now - task.StartDatetime);
+                return;
+            }
+            else return;
+        }
+        private void StartSessionTimer(Session CurrentSession)
+        {
+            DateTime StartInstant = DateTime.Now;
+            if (CurrentSession.ElapsedTime == TimeSpan.Zero)
+            {
+                CurrentSession.StartDatetime = DateTime.Now;
+                return;
+            }
+            else return;
+        }
+        private void StopSessionTimer(Session CurrentSession)
+        {
+            CurrentSession.ElapsedTime = CurrentSession.ElapsedTime + (DateTime.Now - CurrentSession.StartDatetime);
+        }
+        private Session SwitchActiveTask(Session CurrentSession)
+        {
+            if (CurrentSession.ActiveTask != null && CurrentSession.IncompleteTasks.Count() > 0)
+            {
+                CurrentSession.IsActive = false;
+                CurrentSession.IncompleteTasks.Add(CurrentSession.ActiveTask);
+                CurrentSession.ActiveTask = CurrentSession.IncompleteTasks[0];
+                CurrentSession.IncompleteTasks.RemoveAt(0);
+                CurrentSession.ActiveTask.IsActive = true;
+                return session = CurrentSession;
+            }
+            else if (CurrentSession.ActiveTask != null && CurrentSession.IncompleteTasks.Count() == 0)
+            {
+                MessageBox.Show("Only one task, nothing to switch to.");
+                return session = CurrentSession;
+            }
+            else if (CurrentSession.ActiveTask == null && CurrentSession.IncompleteTasks.Count() > 0)
+            {
+                CurrentSession.ActiveTask = CurrentSession.IncompleteTasks[0];
+                CurrentSession.IncompleteTasks.RemoveAt(0);
+                CurrentSession.ActiveTask.IsActive = true;
+                return session = CurrentSession;
+            }
+            else if (CurrentSession.ActiveTask == null && CurrentSession.IncompleteTasks.Count() == 0)
+            {
+                return session = CurrentSession;
+            }
+            return null;
+        }
+        private Session CancelActiveTask(Session CurrentSession)
+        {
+            if (GetActiveTask() != null)
+            {
+                WorkTask CurrentActiveTask = GetActiveTask();
+                CurrentActiveTask.IsCancelled = true;
+                CurrentActiveTask.EndDatetime = DateTime.Now;
+                CurrentSession.CancelledTasks.Add(CurrentActiveTask);
+                CurrentActiveTask.IsActive = false;
+                CurrentSession.ActiveTask = null;
+                return session = CurrentSession;
+            }
+            return null;
+        }
+        private Session CompleteActiveTask(Session CurrentSession)
+        {
+            if (GetActiveTask() != null)
+            {
+                WorkTask CurrentActiveTask = GetActiveTask();
+                Console.WriteLine("Completion function called.");
+                CurrentActiveTask.IsCompleted = true;
+                CurrentActiveTask.IsActive = false;
+                CurrentActiveTask.EndDatetime = DateTime.Now;
+                CurrentSession.CompletedTasks.Add(CurrentActiveTask);
+                CurrentSession.ActiveTask = null;
+                return session = CurrentSession;
+            }
+            else return null;
+        }
+        private void WriteLabels(Session CurrentSession)
+        {
+            WorkTask ActiveTask = null;
+            Project ActiveProject = null;
+
+            if (CurrentSession.ActiveTask != null)
+            {
+                ActiveTask = CurrentSession.ActiveTask;
+                if (ActiveTask.Project != null)
+                {
+                    ActiveProject = ActiveTask.Project;
+                }
+            }
+            if (ActiveTask == null)
+            {
+                activeTaskLabel.Text = "No active task.";
+                activeProjectLabel.Text = "No active project.";
+            }
+            else if (ActiveProject != null)
+            {
+                activeTaskLabel.Text = ActiveTask.Name;
+                activeProjectLabel.Text = ActiveProject.Name;
+            }
+            else
+            {
+                activeTaskLabel.Text = ActiveTask.Name;
+                activeProjectLabel.Text = "No affiliated project.";
+            }
+        }
+        private string CheckStartDatetime(WorkTask task)
+        {
+            if (task.StartDatetime != DateTime.MinValue)
+            {
+                return task.StartDatetime.ToString();
+            }
+            else return "Not Started";
+        }
+        private void Report(Session CurrentSession)
+        {
+            string getStatus(WorkTask task)
+            {
+                if (task.IsCancelled == true)
+                {
+                    return "Cancelled";
+                }
+                else if (task.IsCompleted == true)
+                {
+                    return "Completed";
+                }
+                else
+                {
+                    return "In Progress";
+                }
+            }
+
+                List<WorkTask> AllTasks = new List<WorkTask>();
+            List<WorkTask> CompletedTasks = new List<WorkTask>();
+            List<WorkTask> TasksInProgress = new List<WorkTask>();
+            List<WorkTask> CancelledTasks = new List<WorkTask>();
+
+            string ReportFilePath = @".\goldfishbrainreport.txt";
+            string SaveFilePath = @".\gfbsave.txt"; 
+            string line = "";
+
+
+
+            if (CurrentSession.CompletedTasks.Count > 0)
+            {
+                CompletedTasks = CurrentSession.CompletedTasks;
+            }
+            if (CurrentSession.IncompleteTasks.Count > 0)
+            {
+                TasksInProgress = CurrentSession.IncompleteTasks;
+                if(CurrentSession.ActiveTask != null)
+                {
+                    TasksInProgress.Add(CurrentSession.ActiveTask);
+                }
+            }
+            if (CurrentSession.CancelledTasks.Count > 0)
+            {
+                CancelledTasks = CurrentSession.CancelledTasks;
+            }
+
+            foreach (WorkTask task in CompletedTasks)
+            {
+                AllTasks.Add(task);
+            }
+            foreach (WorkTask task in CancelledTasks)
+            {
+                AllTasks.Add(task);
+            }
+            foreach (WorkTask task in TasksInProgress)
+            {
+                AllTasks.Add(task);
+            }
+
+            line = "Session Information\n Start: " + CurrentSession.StartDatetime + "\t End: " + CurrentSession.EndDatetime + "\t Elapsed: " + CurrentSession.ElapsedTime;
+
+            line = line + ("\n\nTasks") + "\nTask\tStatus\tProject\tSubmitted\tStarted\tStopped\tElapsed";
+
+            foreach (WorkTask task in AllTasks)
+            {
+                Project AffiliatedProject = task.Project;
+
+                if (AffiliatedProject != null)
+                {
+                    line = line + "\n" + task.Name + "\t" + getStatus(task) +"\t"+ AffiliatedProject.Name + "\t" + task.SubmitDatetime + "\t" + CheckStartDatetime(task) + "\t" + task.EndDatetime + "\t" + task.ElapsedTime;
+                }
+                else line = line + "\n" + task.Name + "\t" + getStatus(task) + "\t" + "No Project" + "\t" + task.SubmitDatetime + "\t" + CheckStartDatetime(task) + "\t" + task.EndDatetime + "\t" + task.ElapsedTime;
+            }
+            File.WriteAllText(ReportFilePath, line);
+
+            string WriteActiveTask()
+            {
+                if (GetActiveTask() != null)
+                {
+                    String SessionInfo = CurrentSession.StartDatetime + "|" + CurrentSession.ElapsedTime + "|" + CurrentSession.ActiveTask.Name + "|" + CurrentSession.ActiveTask.StartDatetime + "|" + CurrentSession.ActiveTask.ElapsedTime;
+                    return SessionInfo;
+                }
+
+                else
+                {
+                    String SessionInfo = CurrentSession.StartDatetime + "|" + CurrentSession.ElapsedTime + "|" + "NoActiveTask" + "||";
+                    return SessionInfo;
+                }
+            }
+
+            line =  WriteActiveTask();
+
+            foreach(WorkTask task in AllTasks)
+            {
+                Project AffiliatedProject = task.Project;
+
+                if (AffiliatedProject != null)
+                {
+                    line = line + "\n" + task.Name + "|" + getStatus(task) + "|" + AffiliatedProject.Name + "|" + task.SubmitDatetime + "|" + CheckStartDatetime(task) + "|" + task.EndDatetime + "|" + task.ElapsedTime;
+                }
+                else line = line + "\n" + task.Name + "|" + getStatus(task) + "|" + "noproject" + "|" + task.SubmitDatetime + "|" + CheckStartDatetime(task) + "|" + task.EndDatetime + "|" + task.ElapsedTime;
+                {
+
+                }
+            }
+            File.WriteAllText(SaveFilePath, line);
+        }
+       //private Session LoadSave(String filepath)
+       //{
+       //    List<String> save = File.ReadAllLines(filepath);
+       //    Session NewSession = null;
+       //
+       //    forreach(string)
+       //    if (save.Contains("NoActiveTask") == false)
+       //    {
+       //        NewSession.ActiveTask.Name = save[0];
+       //
+       //    }
+       //
+       //
+       //}
+        private void activeTaskLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void submitTaskAndSwitchButton_Click(object sender, EventArgs e)
+        {
+            StopActiveTaskTimer(session);
+            SubmitAndSwitch(session);
+            StartActiveTaskTimer(session);
+            WriteLabels(session);
+        }
+
+        private void activeTaskLabel_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
